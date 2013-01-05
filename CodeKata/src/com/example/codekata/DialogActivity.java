c1aeeb42fa503cc4101a7061bbfb4509b13d27d1
OferMania <oferhirsch.gill@gmail.com>
2013-01-04 19:03:32 -0800

    Disables tap outside on newer versions for DialogActivity

--- CodeKata/src/com/example/codekata/DialogActivity.java
+++ CodeKata/src/com/example/codekata/DialogActivity.java
@@ -1,9 +1,12 @@
 package com.example.codekata;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager.LayoutParams;
 import android.widget.TextView;
 
 public class DialogActivity extends Activity {
@@ -12,6 +15,11 @@ public class DialogActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+        	setFinishOnTouchOutside(false);
+        }
+        
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.dialog_activity);
         dialogText = (TextView)findViewById(R.id.dialog_text);
@@ -32,5 +40,4 @@ public class DialogActivity extends Activity {
     public void okay(View v) {
         DialogActivity.this.finish();
     }
-
 }
